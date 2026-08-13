9efb7f271faa3b38b86e5fc1206cf36b617db052
Xyra <291816539+Xyra404@users.noreply.github.com>
2026-08-13 14:41:43 +0200

    upd ver

--- raw_converter.py
+++ raw_converter.py
@@ -42,7 +42,7 @@ from PyQt6.QtWidgets import (
     QSlider,
 )
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 class FileItemWidget(QWidget):
     def __init__(self, filename: str, parent=None):
